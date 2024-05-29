ed53ca5d5e6110573ffd87c24bd995cc3eba73bb
Chris Celi <christopher.celi@nist.gov>
2024-05-29 14:47:50 -0400

    RELEASE/v1.6

--- client/start/parsing.py
+++ client/start/parsing.py
@@ -4,7 +4,6 @@ from requests import Response
 from utilities.utils import check_sd_type, log, run_checks
 import json
 import utilities.esvutil as esvutil
-import certifi
 import globalenv
 import utilities.utils as utils
 
@@ -57,13 +56,23 @@ def parse_run(run_path):
         assessment_reg = esvutil.loadObject(run_file[0]["AssessmentRegistrationPath"])
         oeId = []
         rawNoise = []
+        rawNoiseSampleSize = []
         restartTest = []
+        restartSampleSize = []
         conditioned = []
 
         for dataFile in run_file[0]["DataFiles"]:
             oeId.append(dataFile["oeID"])
             rawNoise.append(dataFile["rawNoisePath"])
+            try:
+                rawNoiseSampleSize.append(dataFile["rawNoiseSampleSize"])
+            except Exception:
+                rawNoiseSampleSize.append(-1)
             restartTest.append(dataFile["restartTestPath"])
+            try:
+                restartSampleSize.append(dataFile["restartSampleSize"])
+            except Exception:
+                restartSampleSize.append(-1)
             conditioned.append(dataFile["unvettedConditionedPaths"])
         
         for filepath in rawNoise:
@@ -84,15 +93,23 @@ def parse_run(run_path):
         supporting_paths = [] # run_file[0]["SupportingDocuments"]["filePaths"]
         comments = [] # run_file[0]["SupportingDocuments"]["comments"]
         sdType = [] # run_file[0]["SupportingDocuments"]["sdType"]
+        pud = 0
+        ear = 0
         for supportingDocument in run_file[0]["SupportingDocuments"]:
             supporting_paths.append(supportingDocument["filePath"])
             comments.append(supportingDocument["comment"])
             check_sd_type(supportingDocument["sdType"])
             sdType.append(supportingDocument["sdType"])
-
-        run_checks(comments, sdType, supporting_paths)
-        singleMod = run_file[0]["Assessment"]['limitEntropyAssessmentToSingleModule']
-        entropyId = None; modId = None; vendId = None
+            run_checks(comments, sdType, supporting_paths)
+            if (supportingDocument["sdType"].replace(" ","") == "EntropyAssessmentReport"):
+                ear += 1
+            if(supportingDocument["sdType"].replace(" ","") == "PublicUseDocument"):
+                pud += 1
+        if(ear != 1 or pud != 1):
+            print("Error: In run configuration file, Supporting Documents must contain exactly one EntropyAssessmentReport and exactly one PublicUseDocument")
+            sys.exit(1)
+        limitVendor = run_file[0]["Assessment"]['limitEntropyAssessmentToSingleModule']
+        entropyId = None; modId = None; vendId = None; entropyCertificateToUpdate = None
 
         certify = run_file[0]["Certify"]['Certify'] 
         if certify: #Certification requires module and vendor IDs
@@ -104,18 +121,23 @@ def parse_run(run_path):
             except:
                 print("Error: Entropy, Module and Vendor IDs are required for certification")
                 sys.exit(1)
-        '''
-        if certify and run_file[0]["Assessment"]['numberOfAssessments'] == 1: #Certifying only 1 assessment requires oeID
             try:
-                oeId = run_file[0]["Assessment"]['oeID']
+                entropyCertificateToUpdate = run_file[0]["Certify"]['EntropyCertificateToUpdate']
             except:
-                print("Error: oeID field needed when certifying 1 assessment")
-                sys.exit(1)
-            numberOfOEs  = assessment_reg[1]['numberOfOEs']
-            if len(oeId) != numberOfOEs:
-                print("Error: Number of oeIDs provided must match numberOfOEs in Assessment Registration. numberOfOEs is ", numberOfOEs, " but provided number of oeIDs is ", len(oeId))
-                sys.exit(1)
-                '''
+                # Optional info not found
+                if(globalenv.verboseMode):
+                    print('No updated entropy certificate info found')
+        pudSdId = ""
+        pudEntropyCertificate = ""
+        pudFilePath = ""
+        try: 
+            updatedPud = run_file[0]["UpdatedPublicUseDocument"]
+            pudEntropyCertificate = updatedPud["entropyCertificate"]
+            pudFilePath = updatedPud["filePath"]
+        except:
+            if(globalenv.verboseMode):
+                print('No valid Updated Public Use Document in run config')
+
         responseList = []
         if "PreviousRun" in run_file[0]:
             if(globalenv.verboseMode):
@@ -144,7 +166,7 @@ def parse_run(run_path):
 
         if(globalenv.verboseMode):
             print('Run file successfully parsed')
-        return assessment_reg, rawNoise, restartTest, conditioned, supporting_paths, comments, sdType, modId, vendId, entropyId, oeId, certify, singleMod, responseList
+        return assessment_reg, rawNoise, rawNoiseSampleSize, restartTest, restartSampleSize, conditioned, supporting_paths, comments, sdType, modId, vendId, entropyId, oeId, certify, limitVendor, responseList, pudEntropyCertificate, pudFilePath, entropyCertificateToUpdate
 
     except Exception as e:
         print("There was an error parsing your run file. Please try again")
@@ -154,6 +176,20 @@ def parse_run(run_path):
 def parse_certify_response(response):
     response = response.json()
     status = response[1]['status']
-    messageList = response[1]["information"]["messageList"]
+    try:
+        messageList = response[1]["information"]["messageList"]
+    except:
+        messageList = []
     elementList = response[1]["information"]["entropyAssessmentsReferences"]["elementList"]
     return status, messageList, elementList
+
+def parse_run_sup(run_path):
+    try:
+        run_file = open(run_path, 'r')
+        run_file = json.load(run_file)
+        cert_sup = run_file[0]["PreviousRun"][0]["cert_supp"]
+        return cert_sup
+    except Exception as e:
+        print("There was an error parsing your run file. Please try again")
+        print("Error parsing: ", e)
+        sys.exit(1)
